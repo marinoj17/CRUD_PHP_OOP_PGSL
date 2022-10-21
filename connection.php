83d516117ba1a45abfdbde0f5d652d4f85289adb
marinoj17 <marcosjavier17@gmail.com>
2022-10-21 15:24:17 -0300

    crud oop php con integracion pgsl demo portafolio

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php 
-$host = "localhost";
+$host = "ec2-54-147-36-107.compute-1.amazonaws.com";
 $port = "5432";
-$dbname = "PRUEBA";
-$user = "postgres";
-$password = "123456"; 
+$dbname = "dahtlolq18757k";
+$user = "hnkqyzvfxuigod";
+$password = "476a2877b6cfd4410b7f001feda67092b2f3195d1a2a8449646e88b8a482b5e0"; 
 $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
 $dbconn = pg_connect($connection_string);     
 ?>
